102219bde12e72918fd9f814cee4e0f49396747f
zhu <zhumeng2224@163.com>
2023-09-19 13:08:42 +0100

    backticks in es6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 function App() {
 
-const name="Julie"
-const randomNumber=Math.floor(Math.random()*10)+1
+const fname="Julie"
+const lname="Mish"
+const randomNumber=Math.floor(Math.random()*11)+1
 
   return (
     <div className="App">
-    <h1>hello {name}</h1>
+    <h1>hello {fname+" "+lname}</h1>
+    {/* way2:ES6 template literal:  */}
+    <h1>hello {`${fname} ${lname}`}</h1>
     <p>This is a random number between 1 to 10 with floor method: {randomNumber}</p>
     </div>
   );
