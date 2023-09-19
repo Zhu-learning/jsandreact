22763bdbb63e551acef997ab31abe1423e3c773f
zhu <zhumeng2224@163.com>
2023-09-19 10:50:06 +0100

    practise JSX and generate random number between 1 to 10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+const name="Julie"
+const randomNumber=Math.floor(Math.random()*10)+1
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>hello {name}</h1>
+    <p>This is a random number between 1 to 10 with floor method: {randomNumber}</p>
     </div>
   );
 }
