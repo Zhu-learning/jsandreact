1c853172f628c03a086546933d8378d3199ded74
zhu <zhumeng2224@163.com>
2023-09-19 17:27:04 +0100

    convert css to jsx and style with inline style

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ function App() {
 const fname="Julie"
 const lname="Mish"
 const randomNumber=Math.floor(Math.random()*11)+1
+const currentDate=new Date().getFullYear()
+const randomImage="https://picsum.photos/200"
+const useJsToStyle={
+  color:"red",
+  fontSize:"20px",
+  border:"1px solid black"
+}
+useJsToStyle.color="pink"
 
+
+console.log(currentDate);
   return (
     <div className="App">
     <h1>hello {fname+" "+lname}</h1>
+    <p>this is th ecurrent year: {currentDate}</p>
     {/* way2:ES6 template literal:  */}
-    <h1>hello {`${fname} ${lname}`}</h1>
-    <p>This is a random number between 1 to 10 with floor method: {randomNumber}</p>
+    {/* <h1>hello {`${fname} ${lname}`}</h1> */}
+    <p>This is a random number between 1 to 10 with floor method: {randomNumber} </p>
+    <img src={randomImage + "?grayscale"} alt="this is a random pic in grey"/>
+   <div style={{color:"red"}}>this is inline styling, which inject js to jsx. </div>
+   <div style={useJsToStyle}>this is inline styling method 2, which inject js to jsx. </div>
+   
     </div>
   );
 }
